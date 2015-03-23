be78dad0105a601ce1f1b0cbb55f457c5cc15346
Jelle Widdershoven <jellewiddershoven@gmail.com>
2015-03-23 10:49:26 +0100

    Begin week 5

---
+++ JSF31_w04_KochFractalFX-startup/JSF31KochFractalFX - startup/src/jsf31kochfractalfx/EdgesRunnable.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jsf31kochfractalfx;
+
+/**
+ *
+ * @author Jelle
+ */
+public class EdgesRunnable implements Runnable {
+    
+    public EdgesRunnable() {
+        
+    }
+    
+    @Override
+    public void run() {
+        
+    }
+}
